9af10b618db47f09444a3de4d7bbc45c7301c46b
Ole <ole.ersoy@gmail.com>
2018-03-06 14:40:12 -0600

    Removed unnecessary period and slash

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,49 +2,49 @@ var path = require('path');
 var GLOB = require('glob-patterns');
 
 var PLI = {
-  SOURCE: './src/',
-  TARGET: './target/',
-  DEPLOY: './deploy/',
+  SOURCE: 'src/',
+  TARGET: 'target/',
+  DEPLOY: 'deploy/',
   src: {
     main: {
-      js: './src/main/js/',
-      css: './src/main/css/',
-      html: './src/main/html/',
-      nunjucks: './src/main/nunjucks'
+      js: 'src/main/js/',
+      css: 'src/main/css/',
+      html: 'src/main/html/',
+      nunjucks: 'src/main/nunjucks/'
     },
     test: {
-      js: './src/test/js/',
-      css: './src/test/css/',
-      html: './src/test/html/',
-      nunjucks: './src/test/nunjucks'
+      js: 'src/test/js/',
+      css: 'src/test/css/',
+      html: 'src/test/html/',
+      nunjucks: 'src/test/nunjucks/'
     }
   },
   target: {
     main: {
-      js: './target/main/js/',
-      css: './target/main/css/',
-      html: './target/main/html/',
-      nunjucks: './target/main/nunjucks'
+      js: 'target/main/js/',
+      css: 'target/main/css/',
+      html: 'target/main/html/',
+      nunjucks: 'target/main/nunjucks/'
     },
     test: {
-      js: './target/test/js/',
-      css: './target/test/css/',
-      html: './target/test/html/',
-      nunjucks: './target/test/nunjucks'
+      js: 'target/test/js/',
+      css: 'target/test/css/',
+      html: 'target/test/html/',
+      nunjucks: 'target/test/nunjucks/'
     }
   },
   deploy: {
     main: {
-      js: './deploy/main/js/',
-      css: './deploy/main/css/',
-      html: './deploy/main/html/',
-      nunjucks: './deploy/main/nunjucks'
+      js: 'deploy/main/js/',
+      css: 'deploy/main/css/',
+      html: 'deploy/main/html/',
+      nunjucks: 'deploy/main/nunjucks/'
     },
     test: {
-      js: './deploy/test/js/',
-      css: './deploy/test/css/',
-      html: './deploy/test/html/',
-      nunjucks: './deploy/test/nunjucks'
+      js: 'deploy/test/js/',
+      css: 'deploy/test/css/',
+      html: 'deploy/test/html/',
+      nunjucks: 'deploy/test/nunjucks/'
     }
   }
 
